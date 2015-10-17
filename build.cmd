1707464ccd6795d13507ac2545b05338d961e90e
Henrik Feldt <henrik@haf.se>
2015-10-17 12:09:28 +0200

    Initial State

---
+++ build.cmd
@@ -0,0 +1,3 @@
+tools/paket.bootstrapper.exe
+tools/paket.exe install
+msbuild src/Web.sln
---
+++ src/Web/Program.fs
@@ -0,0 +1,6 @@
+﻿// Learn more about F# at https://fsharp.tv
+
+[<EntryPoint>]
+let main argv =
+  printfn "%A" argv
+  0
