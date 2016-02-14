ecdb7cc0f70c3d5f8401592deda88be443648099
Henrik Feldt <henrik@fsharp.tv>
2016-02-14 12:13:12 +0100

    post 3: routing intro

--- src/Web/Program.fs
+++ src/Web/Program.fs
@@ -2,6 +2,7 @@ open Suave
 
 module HelloWorldModule =
   let okRes = Successful.OK "Hello World!"
+  let bye = Successful.OK "Goodbye World!"
 
 module FilesModule =
   open System
@@ -19,9 +20,32 @@ module FilesModule =
 
   let app =
     Files.browseFile rootPath "files/hello.txt"
- 
+
+
+module RoutingModule =
+  open System.IO
+  open Suave.Filters
+  open Suave.Successful
+  open Suave.Operators
+
+  let private aCat fileName =
+    Path.Combine (FilesModule.rootPath, "cats", fileName)
+
+  let app : WebPart =
+    choose [
+      path "/hi" >=> HelloWorldModule.okRes
+      path "/bye" >=> HelloWorldModule.bye
+      path "/never" >=> never >=> OK "DID RETURN?"
+      path "/never2" >=> (fun ctx -> fail) >=> OK "DID RETURN?"
+      path "/never2" >=> (fun ctx -> async.Return(None : HttpContext option)) >=> OK "DID RETURN?"
+      path "/cat" >=> Files.file (aCat "3.jpg")
+      pathScan "/cats/%i/image" (
+        sprintf "%i.jpg"
+        >> aCat
+        >> Files.file)
+    ]
 
 [<EntryPoint>]
 let main argv =
-  startWebServer defaultConfig FilesModule.cats
+  startWebServer defaultConfig RoutingModule.app
   0
